e133267c48f8f46422e5978e8897eb3d46f2b035
Hash2602 <158342031+hash2602@users.noreply.github.com>
2024-07-30 15:05:26 +0300

    Add files via upload

--- main.sh
+++ main.sh
@@ -2,22 +2,24 @@
 
 echo """
  _____  _______ ___ ___ __  __ ___ _____ ___   ___  _    _  _____ __
-| __\ \/ /_   _| _ \ __|  \/  | __|_   _/ _ \ / _ \| |  | |/ /_ _|_ 
+| __\ \/ /_   _| _ \ __|  \/  | __|_   _/ _ \ / _ \| |  | |/ /_ _|_
 | _| >  <  | | |   / _|| |\/| | _|  | || (_) | (_) | |__| ' < | |  |
 |___/_/\_\ |_| |_|_\___|_|  |_|___| |_| \___/ \___/|____|_|\_\___| |
 MADE BY HASH2602, DREAMTECH
 """
-echo "{1} DOWNLOAD MODULES"
-echo "{2} DTI-MULTI-ATTACK"
-echo "{3} PORT-SCANNER" 
-echo "{4} KALI-TOOLS"
-echo "{5} FAST AND OTO SQLMAP"
-echo "{6} WORMGPT"
-read -p "Lutfen bir secenek secin: " option
+echo "1) DOWNLOAD MODULES"
+echo "2) DTI-MULTI-ATTACK"
+echo "3) PORT-SCANNER"
+echo "4) KALI-TOOLS"
+echo "5) FAST AND AUTO SQLMAP"
+echo "6) WORMGPT"
+echo "7) AUTOAIREPLAY-NG"
+read -p "Lütfen bir seçenek seçin: " option
 
 case $option in
     1)
         bash modules.sh
+        ;;
     2)
         python3 dti-ddos.py
         ;;
@@ -32,7 +34,12 @@ case $option in
         ;;
     6)
         python3 wormgpt.py
+        ;;
+    7)
+        autoaireplay-ng
+        ;;
     *)
-        echo "Gecersiz secenek. Lutfen 1, 2,3 veya 4 giriniz."
+        echo "Geçersiz seçenek. Lütfen 1, 2, 3, 4, 5, 6 veya 7 giriniz."
         ;;
 esac
+
